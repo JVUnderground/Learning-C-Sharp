b269fa732a6fb2f5cb7b3118f53573d3bbc8d85e
João Carneiro <joao.carneiro@gmail.com>
2017-04-07 02:03:23 -0300

    New ignore file

---
+++ Chapter 2/hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Jvunderground {
+
+    public class Hello {
+        static void Main() {
+            Console.WriteLine("Hello World.");
+            Console.ReadLine();
+            return;
+        }
+    }
+}
